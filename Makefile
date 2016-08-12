cf546c395d102ffb6efbd95ee8a041692d4deed2
orina1123 <orina1123@gmail.com>
2016-08-12 15:06:44 +0800

    word similarity evaluation script

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CFLAGS = -lm -pthread -Ofast -march=native -Wall -funroll-loops -Wno-unused-resu
 
 OBJS = EvaluationPP.o Paraphrase.o
 
-PROG = word2vec_save_both word2vec_joint orig_RCM directed_RCM sep_in_out_emb tune_lm eval
+PROG = word2vec_save_both word2vec_joint orig_RCM directed_RCM sep_in_out_emb eval
 all: $(PROG)
 
 %.o : %.cpp
@@ -25,8 +25,8 @@ directed_RCM : directed_RCM.cpp Paraphrase_directed.o
 sep_in_out_emb : sep_in_out_emb.cpp 
 	$(CC) sep_in_out_emb.cpp -o sep_in_out_emb $(CFLAGS)
 
-tune_lm : tune_lm.cpp $(OBJS)
-	$(CC) tune_lm.cpp $(OBJS) -o tune_lm $(CFLAGS)
+#tune_lm : tune_lm.cpp $(OBJS)
+#	$(CC) tune_lm.cpp $(OBJS) -o tune_lm $(CFLAGS)
 
 eval : eval.cpp $(OBJS)
 	$(CC) eval.cpp $(OBJS) -o eval $(CFLAGS)
---
+++ evaluate/eval_word_sim.py
@@ -0,0 +1,71 @@
+#-*- encoding: UTF-8 -*-
+
+import sys
+import codecs
+import re
+import argparse
+import numpy as np
+import gensim
+from scipy.spatial import distance
+from scipy.stats import spearmanr
+from nltk.corpus import wordnet as wn
+
+# settings
+ap = argparse.ArgumentParser()
+ap.add_argument("vec_file", type=str)
+ap.add_argument("test_file", type=str)
+ap.add_argument("-f", "--vec-format", default="word2vec_bin", type=str, help="type of embedding model {word2vec_bin, word2vec_txt, gensim}")
+ap.add_argument("-s", "--sense-expand", default=None, type=str, choices=[None, 'first', 'closest', 'weighted'], help="")
+args = ap.parse_args()
+
+# load word embedding model
+if args.vec_format == "word2vec_bin":
+        model = gensim.models.Word2Vec.load_word2vec_format(args.vec_file, binary=True)
+elif args.vec_format == "word2vec_txt":
+        model = gensim.models.Word2Vec.load_word2vec_format(args.vec_file)
+elif args.vec_format == "gensim":
+        model = gensim.models.Word2Vec.load(args.vec_file)
+
+# read word similarity data
+ans = []
+pred = []
+n_OOV_pair = 0
+with codecs.open(args.test_file, "r", "utf-8") as f:
+        for line in f:
+                parts = re.split(r"\s+", line.strip())
+                #print parts
+                _w1, _w2, score = parts[0].lower(), parts[1].lower(), float(parts[2])
+                
+                w1, w2 = _w1, _w2
+                if args.sense_expand == "first":
+                    syn1 = wn.synsets(_w1)
+                    if len(syn1) > 0:
+                        w1 = syn1[0].name()
+                    syn2 = wn.synsets(_w2)
+                    if len(syn2) > 0:
+                        w2 = syn2[0].name()
+                #TODO other methods of calculating similarity score
+                #print w1, w2, score
+
+                flag = False
+                if w1 not in model:
+                    print "[OOV]", _w1, w1
+                    flag = True
+                if w2 not in model:
+                    print "[OOV]", _w2, w2
+                    flag = True
+                if not flag:
+                    #print model.similarity(w1, w2)
+                    sim = model.similarity(w1, w2)
+                    ans.append(score)
+                    pred.append(sim)
+                else:
+                    print "--skipped--", _w1, _w2, score
+                    n_OOV_pair += 1
+
+cor = spearmanr(ans, pred)[0]
+print "Spearman rank-order correlation coefficient:", cor
+
+print "sequence length: %d" % len(ans)
+print "%d pairs have some word not in the model" % n_OOV_pair
+
